11519bf91fb7c5420a0df3356402adb8b7a8b0c2
macie <macie@users.noreply.github.com>
2023-07-03 09:32:44 +0200

    feat: Specify parent directory
    With this change user can create Go project in directory other than current.

--- tests/test_init.sh
+++ tests/test_init.sh
@@ -2,7 +2,6 @@
 
 beforeAll() {
     TEST_ROOT_DIR=$(mktemp -d -t 'goinit_testXXXXXX')
-    cd "${TEST_ROOT_DIR:-/tmp/goinit}" || exit 1
 }
 
 afterAll() {
@@ -15,10 +14,10 @@ afterAll() {
 #
 
 test_default() {
-    yes '' | goinit 2>/dev/null >&2
+    ./goinit "${TEST_ROOT_DIR}" 2>/dev/null >&2
     test $? -eq 0
 
-    TEST_PROJ_DIR=$(find ./* -type d -prune -print | head -1)
+    TEST_PROJ_DIR=$(find "${TEST_ROOT_DIR}"/* -type d -prune -print | head -1)
     # shellcheck disable=SC2012
     test "$(ls -a "${TEST_PROJ_DIR}" | tr '\n' ' ')" = ". .. .git .gitignore LICENSE README.md go.mod main.go " 
 }
